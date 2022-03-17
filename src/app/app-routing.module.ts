62c8b9e2f69256e85f1012f6146f6b180a48979f
Lucas <lucas43@schule.bremen.de>
2022-03-17 14:09:33 +0100

    SM-19 A_2 Routing zu Endpunkten,Test entfernt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import {MainmenuComponent} from "./mainmenu/mainmenu.component";
-import {TestComponent} from "./test/test.component";
 
 
 const routes: Routes = [
 
   { path: 'mainmenu', component:MainmenuComponent },
-  { path: '',redirectTo:'/mainmenu', pathMatch:'full'},
-  {path: 'test',component:TestComponent}
+  { path: '',redirectTo:'/mainmenu', pathMatch:'full'}
 ];
 
 @NgModule({
