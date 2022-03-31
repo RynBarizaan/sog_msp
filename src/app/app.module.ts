fe0e9f31ec49d322aedb526582c3909d63073863
AboushH <haneef.aboush@3txpert.com>
2022-03-31 12:15:57 +0200

    SM-12 Gruppe bearbeiten/SM-10 Gruppe von Person anlegen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MainmenuComponent } from './mainmenu/mainmenu.component';
+import { GroupTableComponent } from './group-table/group-table.component';
+import { SliderComponent } from './slider/slider.component';
+import {MatSliderModule} from "@angular/material/slider";
+import {MatCardModule} from "@angular/material/card";
+import {MatNativeDateModule} from '@angular/material/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {Ng5SliderModule} from "ng5-slider";
+import {MatInputModule} from "@angular/material/input";
+import {RouterModule} from "@angular/router";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainmenuComponent
+    MainmenuComponent,
+    GroupTableComponent,
+    SliderComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatSliderModule,
+    MatCardModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    Ng5SliderModule,
+    MatInputModule,
+    RouterModule.forRoot([
+      {path:'', component:SliderComponent},
+      {path: 'groupTable/:value', component:GroupTableComponent}
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
